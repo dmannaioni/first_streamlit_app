6d4b9142e333cf9951a0c1ae5a38d06e3496c244
Daniel Mannaioni <74573418+dmannaioni@users.noreply.github.com>
2022-07-14 16:24:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,5 +63,3 @@ if streamlit.button('Add a Fruit to the List'):
    back_from_function = insert_row_snowflake(add_my_fruit)
    my_cnx.close()
    streamlit.text(back_from_function)
-
-    Add a Fruit to the
